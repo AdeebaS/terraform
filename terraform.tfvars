2e9df6636f37ea6bee59367c748cd35010e3f493
AdeebaS <adeebas0811@gmail.com>
2022-02-13 19:43:49 +0000

    adding terraform files

---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+image_id = "ami-033b95fb8079dc481"
+
+subnet_id = "subnet-0741add072b5ddc2b"
+
+vpc_sg = "sg-067c85b48c9f32a24"
+
+key_file = "Adeeba2-aws"
+
+instance = "t2.micro"
+
+monitor = "true"
+
+bucket = "adeebas3"
---
+++ vars.tf
@@ -0,0 +1,35 @@
+variable "image_id" {
+  type        = string
+  description = "ami"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "subnet_id"
+}
+
+variable "vpc_sg" {
+  type        = string
+  description = "vpc_security_group"
+}
+
+variable "key_file" {
+  type        = string
+  description = "pem_key"
+}
+
+variable "instance" {
+  type        = string
+  description = "instance_type"
+}
+
+variable "monitor" {
+  type        = string
+  description = "monitoring enabled true or false"
+}
+
+variable "bucket" {
+  type        = string
+  description = "s3 bucket name"
+}
+
